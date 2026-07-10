53f4ca1f900d42bfd294f452ed06010b7b9bd4d6
klausimaus75-boop <271047108+klausimaus75-boop@users.noreply.github.com>
2026-07-10 22:06:43 +0200

    Remove login gate for site preview

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,19 @@ import { ProjectFormPage } from "./modules/projects/ProjectFormPage";
 import { ProjectDetailPage } from "./modules/projects/ProjectDetailPage";
 import { BookConceptStepPage } from "./modules/projects/BookConceptStepPage";
 import { SettingsPage } from "./modules/settings/SettingsPage";
-import { AuthGate } from "./modules/auth/AuthGate";
 
 export function App() {
   return (
-    <AuthGate>
-      <AppLayout>
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/projects/new" element={<ProjectFormPage mode="create" />} />
-          <Route path="/projects/:projectId" element={<ProjectDetailPage />} />
-          <Route path="/projects/:projectId/edit" element={<ProjectFormPage mode="edit" />} />
-          <Route path="/projects/:projectId/steps/book-concept" element={<BookConceptStepPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </AppLayout>
-    </AuthGate>
+    <AppLayout>
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/projects/new" element={<ProjectFormPage mode="create" />} />
+        <Route path="/projects/:projectId" element={<ProjectDetailPage />} />
+        <Route path="/projects/:projectId/edit" element={<ProjectFormPage mode="edit" />} />
+        <Route path="/projects/:projectId/steps/book-concept" element={<BookConceptStepPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </AppLayout>
   );
 }
